498ff40a02f85238c23bfa613ea4f665c0b0bb59
natasasdj <natasa.sarafijanovic@gmail.com>
2017-06-20 11:34:25 +0200

    modified images_sqlite

--- analysis/02_images_sqlite.py
+++ analysis/02_images_sqlite.py
@@ -2,196 +2,251 @@ import sys
 import os
 import sqlite3
 import pandas as pd
-import numpy as np
-from PIL import Image
-#import os.path
-import imghdr
-import logging
-#logging.basicConfig()
-from PIL import ImageFile
-import magic
 from urlparse import urlparse
-import rsvg
-import cairo
-#To ensure that *.png file are read
-#ImageFile.LOAD_TRUNCATED_IMAGES = True
+from magic import from_file as magic_from_file
+import re
+import xml.etree.ElementTree as ET
+from PIL import Image
+from timeit import default_timer as timer
+
 
 data_dir = sys.argv[1]
-no_db = int(data_dir[data_dir.find("_")+1:-3])
-
-curr_dir = os.getcwd()
-data_img_dir = data_dir+'httpResp/'
-res_dir = curr_dir + '/results/'
+print data_dir
+start_site = data_dir.split("_")[1]
+data_img_dir = os.path.join(data_dir,'httpResp')
+print data_img_dir
+res_dir = sys.argv[2]
 if not os.path.exists(res_dir):
     os.makedirs(res_dir)
+print res_dir
 
-db = res_dir + 'images.sqlite'
+db = os.path.join(res_dir,'images_' + start_site + '.sqlite') #sys.argv[1]
+print db
 conn1 = sqlite3.connect(db)
 cur1 = conn1.cursor()
 #cur1.execute('DROP TABLE IF EXISTS Images')
 
 cur1.execute('CREATE TABLE IF NOT EXISTS Images (site_id INTEGER NOT NULL, link_id INTEGER NOT NULL, resp_id INTEGER NOT NULL, \
-                                  resp_domain INTEGER NOT NULL, resp_domain2 INTEGER NOT NULL,size INTEGER NOT NULL, cont_length INTEGER NOT NULL, \
+                                  resp_domain INTEGER NOT NULL, size INTEGER, cont_length INTEGER, \
                                   type INTEGER,  cont_type INTEGER, pixels INTEGER,  \
                                   PRIMARY KEY (site_id, link_id, resp_id), \
-                                  FOREIGN KEY (site_id) REFERENCES Domains(id),\
                                   FOREIGN KEY (resp_domain) REFERENCES Domains(id),\
-                                  FOREIGN KEY (resp_domain2) REFERENCES DomainsTwoPart(id),\
-                                  FOREIGN KEY (type) REFERENCES Types(id) )')
+                                  FOREIGN KEY (type) REFERENCES Types(id))')
 
 
-cur1.execute('DROP TABLE IF EXISTS Types')
 cur1.execute('CREATE TABLE IF NOT EXISTS Types (id INTEGER PRIMARY KEY NOT NULL, type TEXT UNIQUE)')
 
+db = res_dir + 'domains.sqlite'
+conn2 = sqlite3.connect(db)
+cur2 = conn2.cursor()
+
+db= os.path.join(data_dir,'crawl-data.sqlite')
+conn = sqlite3.connect(db)
 
 
-no_sites = 100
-max_no_links = 300
-db_file=data_dir+'crawl-data.sqlite'
-print db_file
-conn = sqlite3.connect(db_file)
 
-def findAll(s, ch):
-    return [i for i, ltr in enumerate(s) if ltr == ch]
+ts = timer()
+
+query = 'SELECT * FROM site_visits WHERE (link_id = 0 AND resp_time_2 IS NOT NULL) OR (link_id != 0 AND resp_time_3 IS NOT NULL) ORDER BY site_id ASC, link_id ASC'
+df1 = pd.read_sql_query(query,conn)
+print "start 2"
+query = 'SELECT * FROM http_responses'
+df2 = pd.read_sql_query(query,conn)
+print "start 3"
+df = df1.merge(df2, on = ('site_id','link_id'),how='left')
+
+k = 0 
+for index, row in df.iterrows():
+    # print row['site_id'],row['link_id'], row['response_id'] 
+    if pd.isnull(row['file_name']): continue
+    if 'html' in row['file_name']:  continue
+    header = row['headers']
+    # content-length
+    i1 = header.find('"Content-Length",') 
+    i2 = header.find('"', i1+len('"Content-Length"'))
+    i3 = header.find('"', i2+1) 
+    cont_length = header[i2+1:i3]
+    # content-type
+    i1 = header.find('"Content-Type",') 
+    i2 = header.find('"', i1+len('"Content-Type"'))
+    i3 = header.find('"', i2+1) 
+    cont_type = header[i2+1:i3].lower()
+    cont_type = re.sub("[,;].*","",cont_type)
+   
+    cur1.execute('SELECT id FROM Types WHERE type = ?',(cont_type,))
+    try:
+        cont_type_id = cur1.fetchone()[0]
+    except:
+        cur1.execute('INSERT INTO Types (id,type) VALUES (?,?)',(None,cont_type))
+        cont_type_id = cur1.lastrowid
+    # response domain    
+    url = row['url']
+    domain = urlparse(url).hostname.strip('www.')
     
-def twoPartDomain(domain):
-    points=findAll(domain,'.')
-    if len(points)<2: return domain
-    return domain[points[-2]+1:]
-
-#filename = res_dir +'no_links.txt'
-#f1 = open(filename,'a')
-#filename = res_dir +'img_sizes.txt'
-#f2 = open(filename,'a')
-for i in range(no_db*100+1,no_db*100+no_sites+1):    
-    succ = 0
-    j = 0
-    print i
-    query = 'SELECT * FROM site_visits WHERE site_id = {0} AND link_id = {1}'.format(i, j)
-    df = pd.read_sql_query(query,conn)
-    if pd.isnull(df.loc[0,'resp_time_3']): continue   
-    s = str(i) + ' ' + str(df.loc[0,'no_links']) + ' '       
-    #f1.write(s)
-
-
-    for j in range(0,max_no_links+1):
-        #print i, j
-        if j!=0:  
-            query = 'SELECT * FROM site_visits WHERE site_id = {0} AND link_id = {1}'.format(i, j)
-            df = pd.read_sql_query(query,conn)
-        if len(df.index)==0: break
-        if pd.isnull(df.loc[0,'resp_time_2']):
-            # j += 1 
-            continue
-        succ += 1              
-        query = 'SELECT * FROM http_responses WHERE site_id = {0} AND link_id = {1}'.format(i, j)
-        df = pd.read_sql_query(query,conn)  
-
-        #f4 = open(filename,'a')
-        for index, row in df.iterrows():
-            if pd.isnull(row['file_name']): continue
-            if 'html' in row['file_name']:  continue
-            header = row['headers']
-            i1 = header.find('"Content-Length",') 
-            i2 = header.find('"', i1+len('"Content-Length"'))
-            i3 = header.find('"', i2+1) 
-            cont_length = header[i2+1:i3]
-            i1 = header.find('"Content-Type",') 
-            i2 = header.find('"', i1+len('"Content-Type"'))
-            i3 = header.find('"', i2+1) 
-            cont_type = header[i2+1:i3]
-            url = row['url']
-            domain = urlparse(url).hostname.strip('www.')
-            
-            filename = data_img_dir +'site-' + str(i) + '/' + row['file_name']
-            img_type = magic.from_file(filename, mime=True)
-            cur1.execute('SELECT id FROM Types WHERE type = ?',(img_type,))
+    cur1.execute('SELECT id FROM Domains WHERE domain = ?',(domain,)) 
+    try:
+       domain_id = cur1.fetchone()[0]
+    except:
+       cur1.execute('INSERT INTO Domains (id,domain) VALUES (?,?)',(None,domain))
+       domain_id = cur1.lastrowid
+    # file size and no of pixels
+    filename = os.path.join(data_img_dir,'site-' + str(row['site_id']),row['file_name'])
+    no_pixels = None; size = None; type_id = None
+    try:
+        size = os.path.getsize(filename)
+        img_type = magic_from_file(filename, mime=True).lower()
+        img_type = re.sub("[,;].*","",img_type)      
+        try: 
+            img = Image.open(filename)
+            width, height = img.size
+            no_pixels = width*height
+        except:           
             try:
-               type_id = cur1.fetchone()[0]
+                height = ET. parse(filename).getroot().attrib["height"]
+                height = int(re.sub("[^0-9]", "", height))
+                width = ET.parse(filename).getroot().attrib["width"] 
+                width = int(re.sub("[^0-9]", "", width))
+                no_pixels = height * width
             except:
-               cur1.execute('INSERT INTO Types (id,type) VALUES (?,?)',(None,img_type))
-               type_id = cur1.lastrowid
-            if img_type in cont_type: cont_type = img_type
-            cur1.execute('SELECT id FROM Types WHERE type = ?',(cont_type,))
-            try:
-               cont_type_id = cur1.fetchone()[0]
-            except:
-               cur1.execute('INSERT INTO Types (id,type) VALUES (?,?)',(None,cont_type))
-               cont_type_id = cur1.lastrowid
-            cur1.execute('SELECT id FROM Domains WHERE domain = ?',(domain,)) 
-            try:
-               domain_id = cur1.fetchone()[0]
-            except:
-               cur1.execute('INSERT INTO Domains (id,domain) VALUES (?,?)',(None,domain))
-               domain_id = cur1.lastrowid
-            domainTwoPart = twoPartDomain(domain)
-            cur1.execute('SELECT id FROM DomainsTwoPart WHERE domainTwoPart = ?',(domainTwoPart,)) 
-            try:
-               domainTwoPart_id = cur1.fetchone()[0]
-            except:
-               cur1.execute('INSERT INTO DomainsTwoPart (domainTwoPart) VALUES (?)',(domainTwoPart,))
-               domainTwoPart_id = cur1.lastrowid   
-            size = os.path.getsize(filename)
-            #print i,j, cont_length, size, cont_type, img_type  
-            #print url, domain
-            #print type_id, domain_id
-            # if img_type not in cont_type: print img_type, cont_type
-            no_pixels = None
-            try: 
-                img = Image.open(filename)
-                width, height = img.size
-                no_pixels = width*height
-            except:           
-               try:
-                  svg = rsvg.Handle(file=filename)
-                  width = svg.props.width
-                  height = svg.props.height
-                  no_pixels = width * height
-               except: 
-                  pass
-                    
-            #print (row['site_id'], row['link_id'], row['response_id'], domain_id, size, cont_length, type_id, cont_type_id, no_pixels)
-            #cur1.execute('INSERT IF NOT EXISTS INTO Images (site_id, link_id, resp_id) VALUES (?, ?, ?)', (row['site_id'], row['link_id'], row['response_id'])) 
-            cur1.execute('INSERT INTO Images (site_id, link_id, resp_id, resp_domain, resp_domain2, size, cont_length, type, cont_type, pixels) \
-                         VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)', (row['site_id'], row['link_id'], row['response_id'], domain_id, size, cont_length, type_id, cont_type_id, no_pixels)) 
-    conn1.commit()
-conn1.close()
-conn.close()    
-'''
-            
-            
-            filename = data_img_dir +'site-' + str(i) + '/' + row['file_name']
-            
-         
-                
-            
-                           
-            s1 = str(k) + ' ' + str(row['site_id'])+ ' ' + str(row['link_id'])+ ' ' + str(row['response_id'])+' ' + str(size) +' ' + str(length) + ' '
-            s2 = str(no_pixels) + ' ' + str(img_type) + ' ' + img_format + ' ' + str() + '\n'
-            print s1,s2
-            
-            curr
+                pass
 
-
-            #f2.write(s1)
-            #f2.write(s2)
-            #f3.write(s)
-            #f4.write(s)
+        if img_type in cont_type: cont_type = img_type
+        if 'jpg' in cont_type: cont_type = 'image/jpeg'
+        if 'bmp' in cont_type: cont_type = 'image/bmp'
+        if 'bitmap' in cont_type: cont_type = 'image/bmp'
+        if 'bmp' in img_type: img_type = 'image/bmp'
+        if 'bitmap' in img_type: img_type = 'image/bmp'           
+        if 'icon' in cont_type: cont_type = 'image/x-icon'
+        if 'icon' in img_type: img_type = 'image/x-icon'           
         
-        #f4.close()
-         
-        # j += 1
-    #f3.close()
-    s=str(succ)+'\n'
-    #f1.write(s)     
+        cur1.execute('SELECT id FROM Types WHERE type = ?',(img_type,))
+        try:
+            type_id = cur1.fetchone()[0]
+        except:
+            cur1.execute('INSERT INTO Types (id,type) VALUES (?,?)',(None,img_type))
+            type_id = cur1.lastrowid
 
-#f1.close()
-#f2.close()
-    
+        
+   
+    except:
+        pass
+
+    cur1.execute('INSERT INTO Images (site_id, link_id, resp_id, resp_domain, size, cont_length, type, cont_type, pixels) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)', (row['site_id'], row['link_id'], row['response_id'], domain_id, size, cont_length, type_id, cont_type_id, no_pixels)) 
+                 
+    k += 1
+    if k % 100:
+        print row['site_id']
+        conn1.commit()
+
+conn1.commit()
+
+
+
+
+
 '''
+for index, row in df.iterrows():
+    print row['site_id'],row['link_id']
+    t1 = timer()
+    #if row['site_id']>2: break    
+    if row['link_id'] ==0:
+        if pd.isnull(row['resp_time_3']): continue
+    else:
+        if pd.isnull(row['resp_time_2']): continue
+    #s = str(row['site_id']) + ' ' + str(df.loc[0,'no_links']) + ' '               
+    df2 = df1.loc[(df1['site_id'] == row['site_id']) & (df1['link_id'] == row['link_id'])]
+    #print df2 
+    t2 = timer()
+    for index2, row2 in df2.iterrows():
+        #print row2
+        t3 = timer()
+        if pd.isnull(row2['file_name']): continue
+        if 'html' in row2['file_name']:  continue
+        header = row2['headers']
+        i1 = header.find('"Content-Length",') 
+        i2 = header.find('"', i1+len('"Content-Length"'))
+        i3 = header.find('"', i2+1) 
+        cont_length = header[i2+1:i3]
+        i1 = header.find('"Content-Type",') 
+        i2 = header.find('"', i1+len('"Content-Type"'))
+        i3 = header.find('"', i2+1) 
+        cont_type = header[i2+1:i3].lower()
+        cont_type = re.sub("[,;].*","",cont_type)
+        url = row2['url']
+        domain = urlparse(url).hostname.strip('www.')
+        filename = os.path.join(data_img_dir,'site-' + str(row2['site_id']),row2['file_name'])
+        img_type = magic_from_file(filename, mime=True).lower()
+        img_type = re.sub("[,;].*","",img_type)
+        #if img_type != cont_type and ('svg' not in cont_type):
+            #print img_type, cont_type
+        no_pixels = None
+        try: 
+            img = Image.open(filename)
+            width, height = img.size
+            no_pixels = width*height
+        except: 
+            #if 'svg' in img_type: print 10*"svg"             
+            try:
+                height = ET. parse(filename).getroot().attrib["height"]
+                height = int(re.sub("[^0-9]", "", height))
+                width = ET.parse(filename).getroot().attrib["width"] 
+                width = int(re.sub("[^0-9]", "", width))
+                no_pixels = height * width
+            except:
+                pass
+            #print row2['site_id'],row2['link_id'], row2['response_id'], 'no_pixels:', no_pixels, img_type, cont_type, t
+        
+        #print img_type
+        if img_type in cont_type: cont_type = img_type
+        if 'jpg' in cont_type: cont_type = 'image/jpeg'
+        if 'bmp' in cont_type: cont_type = 'image/bmp'
+        if 'bitmap' in cont_type: cont_type = 'image/bmp'
+        if 'bmp' in img_type: img_type = 'image/bmp'
+        if 'bitmap' in img_type: img_type = 'image/bmp'           
+        if 'icon' in cont_type: cont_type = 'image/x-icon'
+        if 'icon' in img_type: img_type = 'image/x-icon'           
+        
+        t4 = timer()
+        
+        cur1.execute('SELECT id FROM Types WHERE type = ?',(img_type,))
+        try:
+            type_id = cur1.fetchone()[0]
+        except:
+            cur1.execute('INSERT INTO Types (id,type) VALUES (?,?)',(None,img_type))
+            type_id = cur1.lastrowid
+
+        cur1.execute('SELECT id FROM Types WHERE type = ?',(cont_type,))
+        try:
+            cont_type_id = cur1.fetchone()[0]
+        except:
+            cur1.execute('INSERT INTO Types (id,type) VALUES (?,?)',(None,cont_type))
+            cont_type_id = cur1.lastrowid
+        cur2.execute('SELECT id FROM Domains WHERE domain = ?',(domain,)) 
+        try:
+           domain_id = cur2.fetchone()[0]
+        except:
+           domain_id = None
+        size = os.path.getsize(filename)
+
+        cur1.execute('INSERT INTO Images (site_id, link_id, resp_id, resp_domain, size, cont_length, type, cont_type, pixels) \
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)', (row2['site_id'], row2['link_id'], row2['response_id'], domain_id, size, cont_length, type_id, cont_type_id, no_pixels)) 
+        t5 = timer()
+        f2.write(str(row2['site_id']) + ' ' + str(row2['link_id']) + ' ' + str(row2['response_id']) + ' '  + str(t3-t2) + ' '  + str(t4-t3) + ' '   + str(t5-t4) + '\n'   )
+    f1.write(str(row['site_id']) + ' '  + str(row['link_id']) + ' '  + str(t2-t1) + '\n'  )           
+    conn1.commit()
+    conn2.commit()
+    
+'''    
+te = timer()
+print "time:", te - ts
+
+conn1.close()
+conn2.close()
+conn.close()  
 
 
                     
+                   
          
                 
         
